0c592d91e7fad95f0cbb82038028bac1ed9e5834
Christopher Ogden <ogdenchris@gmail.com>
2014-02-22 18:32:53 -0500

    Now can access API through Python via JavaScript initiated cookie

--- Chris/app.py
+++ Chris/app.py
@@ -1,6 +1,20 @@
+import facebook
+import secrets
+import requests
 from flask import Flask, redirect, request, render_template
 app = Flask(__name__)
+
+APP_ID = "214736522060342"
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/_get_cookie")
+def get_cookie():
+    cookie = request.cookies
+    user = facebook.get_user_from_cookie(cookie, APP_ID, secrets.APP_SECRET)
+    graph = facebook.GraphAPI(user['access_token'])
+    user = graph.get_object("me")
+    posts = graph.request('/me/feed')
+    return str(posts)
