65bedb28a94b04119b485d0b0282575f3419dbf1
paine.hanne@gmail.com <paine.hanne@gmail.com>
2014-02-22 22:21:58 -0500

    Added missing files

--- Chris/app.py
+++ Chris/app.py
@@ -4,16 +4,14 @@ import requests
 from flask import Flask, redirect, request, render_template
 app = Flask(__name__)
 
-APP_ID = "214736522060342"
-
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', APP_ID=secrets.APP_ID)
 
 @app.route("/_get_cookie")
 def get_cookie():
     cookie = request.cookies
-    user = facebook.get_user_from_cookie(cookie, APP_ID, secrets.APP_SECRET)
+    user = facebook.get_user_from_cookie(cookie, secrets.APP_ID, secrets.APP_SECRET)
     graph = facebook.GraphAPI(user['access_token'])
     user = graph.get_object("me")
     posts = graph.request('/me/feed')
